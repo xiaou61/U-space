eefb730607a914a53347c22e8bface0fea51b68b
aliyun1153623907 <3153566913@qq.com>
2025-05-23 12:53:02 +0800

    feat: 集成redis模块 redisson

---
+++ xiaou-common/xiaou-common-redis/src/main/java/com/xiaou/properties/RedissonProperties.java
@@ -0,0 +1,17 @@
+package com.xiaou.properties;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+
+@ConfigurationProperties(prefix = "spring.redis.redisson")
+@Data
+@Component
+public class RedissonProperties {
+    private String config;
+
+    private String file;
+
+}
