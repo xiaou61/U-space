22ff2cf3c456cf50a35d33ecb190ea770fc2db7d
aliyun1153623907 <3153566913@qq.com>
2025-05-23 16:25:17 +0800

    feat: 学院添加修复

--- xiaou-modules/xiaou-userinfo/src/main/java/com/xiaou/userinfo/service/impl/CollegeServiceImpl.java
+++ xiaou-modules/xiaou-userinfo/src/main/java/com/xiaou/userinfo/service/impl/CollegeServiceImpl.java
@@ -1,20 +1,25 @@
-package com.xiaou.userinfo.service;
+package com.xiaou.userinfo.service.impl;
 
 import cn.dev33.satoken.stp.StpUtil;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.xiaou.common.domain.R;
-import com.xiaou.userinfo.domain.User;
+
 import com.xiaou.userinfo.domain.bo.UCollegeBO;
 import com.xiaou.userinfo.domain.entity.College;
 import com.xiaou.userinfo.domain.vo.UCollegeVO;
 import com.xiaou.userinfo.mapper.CollegeMapper;
+import com.xiaou.userinfo.service.CollegeService;
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
 @Service
+@Slf4j
 public class CollegeServiceImpl extends ServiceImpl<CollegeMapper, College> implements CollegeService {
 
     @Resource
@@ -26,15 +31,14 @@ public class CollegeServiceImpl extends ServiceImpl<CollegeMapper, College> impl
         College college = new College();
         BeanUtils.copyProperties(collegeBO, college);
         college.setCreatedTime(LocalDateTime.now());
+        Object obj = StpUtil.getSession().get("user_login");
+        // 转换为 JSONObject
+        JSONObject jsonObject = JSON.parseObject(JSON.toJSONString(obj));
+        // 提取 username
+        String username = jsonObject.getString("username");
+        college.setCreatedBy(username);
 
 
-        Object userObj = StpUtil.getSession().get("user_login");
-        String createdBy = "admin123";  // 默认admin
-        if (userObj instanceof User user && user.getUsername() != null && !user.getUsername().isBlank()) {
-            createdBy = user.getUsername();
-        }
-
-        college.setCreatedBy(createdBy);
         collegeMapper.insert(college);
         return R.ok(UCollegeVO.fromEntity(college));
     }
