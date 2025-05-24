ae2cc82f0ecf106e694c5fd8ebc654d616b40681
aliyun1153623907 <3153566913@qq.com>
2025-05-24 16:54:03 +0800

    feat: 校园指南CURD

--- xiaou-modules/xiaou-campus/src/main/java/com/xiaou/campus/controller/CampusGuideController.java
+++ xiaou-modules/xiaou-campus/src/main/java/com/xiaou/campus/controller/CampusGuideController.java
@@ -7,6 +7,8 @@ import com.xiaou.campus.service.CampusGuideService;
 import com.xiaou.common.domain.R;
 import com.xiaou.common.page.PageReqDto;
 import com.xiaou.common.page.PageRespDto;
+import com.xiaou.log.annotation.Log;
+import com.xiaou.log.enums.BusinessType;
 import jakarta.annotation.Resource;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -24,6 +26,7 @@ public class CampusGuideController {
     /**
      * 新增校园指南
      */
+    @Log(title = "新增校园指南",businessType = BusinessType.INSERT)
     @PostMapping("/add")
     public R<CampusGuideVO> addGuide(@RequestBody CampusGuideBO campusGuideBO) {
         return campusGuideService.addGuide(campusGuideBO);
@@ -31,6 +34,7 @@ public class CampusGuideController {
     /**
      * 更新校园指南
      */
+    @Log(title = "更新校园指南",businessType = BusinessType.UPDATE)
     @PutMapping("/update/{id}")
     public R<CampusGuideVO> updateGuide(@PathVariable Long id, @RequestBody CampusGuideBO campusGuideBO) {
         return campusGuideService.updateGuide(id, campusGuideBO);
@@ -39,6 +43,7 @@ public class CampusGuideController {
     /**
      * 删除校园指南（逻辑删除）
      */
+    @Log(title = "删除校园指南",businessType = BusinessType.DELETE)
     @DeleteMapping("/delete/{id}")
     public R<String> deleteGuide(@PathVariable Long id) {
         return campusGuideService.deleteGuide(id);
