6a02f264219b33fb85ec928e94fc02fd429602ae
aliyun1153623907 <3153566913@qq.com>
2025-05-24 12:55:13 +0800

    fix:优化

--- xiaou-modules/xiaou-userinfo/src/main/java/com/xiaou/userinfo/domain/vo/UMajorVO.java
+++ xiaou-modules/xiaou-userinfo/src/main/java/com/xiaou/userinfo/domain/vo/UMajorVO.java
@@ -4,6 +4,8 @@ import com.xiaou.userinfo.domain.entity.Major;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @Builder
 public class UMajorVO {
@@ -15,6 +17,10 @@ public class UMajorVO {
     private String educationLevel;
     private String status;
     private String collegeName; // 新增字段
+    private String createdBy;        // 创建人
+    private String updatedBy;        // 更新人
+    private LocalDateTime createdTime;  // 创建时间
+    private LocalDateTime updatedTime;  // 更新时间
 
     public static UMajorVO fromEntity(Major major, String collegeName) {
         return UMajorVO.builder()
@@ -26,6 +32,10 @@ public class UMajorVO {
                 .educationLevel(major.getEducationLevel())
                 .status(major.getStatus())
                 .collegeName(collegeName)
+                .createdBy(major.getCreatedBy())
+                .updatedBy(major.getUpdatedBy())
+                .createdTime(major.getCreatedTime())
+                .updatedTime(major.getUpdatedTime())
                 .build();
     }
 }
