1ce7f8937e38155a6e1549a7cfd663d8c282169c
aliyun1153623907 <3153566913@qq.com>
2025-05-25 14:32:15 +0800

    fix: 修复模块问题

--- xiaou-common/xiaou-common-core/src/main/java/com/xiaou/common/config/ApplicationConfig.java
+++ xiaou-common/xiaou-common-core/src/main/java/com/xiaou/common/config/ApplicationConfig.java
@@ -1,12 +1,14 @@
 package com.xiaou.common.config;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @AutoConfiguration
 @EnableAspectJAutoProxy
 @EnableAsync(proxyTargetClass = true)
+@EnableConfigurationProperties
 public class ApplicationConfig {
 
 }
