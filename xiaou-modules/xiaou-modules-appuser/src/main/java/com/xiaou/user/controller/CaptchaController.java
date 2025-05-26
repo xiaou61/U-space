df1d4be2d1e61acd52d32f25153a8154d9b5d44f
aliyun1153623907 <3153566913@qq.com>
2025-05-26 16:25:54 +0800

    feat: 邮箱接口验证码

--- xiaou-modules/xiaou-modules-appuser/src/main/java/com/xiaou/user/controller/CaptchaController.java
+++ xiaou-modules/xiaou-modules-appuser/src/main/java/com/xiaou/user/controller/CaptchaController.java
@@ -3,18 +3,23 @@ package com.xiaou.user.controller;
 import cn.hutool.captcha.AbstractCaptcha;
 import cn.hutool.captcha.generator.CodeGenerator;
 import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.RandomUtil;
 import com.xiaou.common.constant.GlobalConstants;
 import com.xiaou.common.domain.R;
+import com.xiaou.common.exception.ServiceException;
 import com.xiaou.common.utils.ReflectUtils;
 import com.xiaou.common.utils.SpringUtils;
 import com.xiaou.common.utils.StringUtils;
 import com.xiaou.config.CaptchaProperties;
+import com.xiaou.mail.config.MailProperties;
+import com.xiaou.mail.utils.MailUtils;
 import com.xiaou.ratelimiter.annotation.RateLimiter;
 import com.xiaou.ratelimiter.enums.LimitType;
 import com.xiaou.user.domain.vo.CaptchaVo;
 import com.xiaou.enums.CaptchaType;
 import com.xiaou.utils.RedisUtils;
 import jakarta.annotation.Resource;
+import jakarta.validation.constraints.NotBlank;
 import lombok.extern.slf4j.Slf4j;
 
 
@@ -36,6 +41,9 @@ public class CaptchaController {
 
     @Resource
     private CaptchaProperties captchaProperties;
+    @Resource
+    private MailProperties mailProperties;
+
     /**
      * 生成验证码
      */
@@ -49,6 +57,39 @@ public class CaptchaController {
         }
         return R.ok(SpringUtils.getAopProxy(this).getCodeImpl());
     }
+
+
+    /**
+     * 邮箱验证码
+     *
+     * @param email 邮箱
+     */
+    @GetMapping("/email/code")
+    public R<Void> emailCode(@NotBlank(message = "{user.email.not.blank}") String email) {
+        if (!mailProperties.getEnabled()) {
+            return R.fail("当前系统没有开启邮箱功能！");
+        }
+        SpringUtils.getAopProxy(this).emailCodeImpl(email);
+        return R.ok();
+    }
+
+    /**
+     * 邮箱验证码
+     * 独立方法避免验证码关闭之后仍然走限流
+     */
+    @RateLimiter(key = "#email", time = 60, count = 1)
+    public void emailCodeImpl(String email) {
+        String key = GlobalConstants.CAPTCHA_CODE_KEY + email;
+        String code = RandomUtil.randomNumbers(4);
+        RedisUtils.setCacheObject(key, code, Duration.ofMinutes(2));
+        try {
+            MailUtils.sendText(email, "登录验证码", "您本次验证码为：" + code + "，有效性为" + 2 + "分钟，请尽快填写。");
+        } catch (Exception e) {
+            log.error("验证码短信发送异常 => {}", e.getMessage());
+            throw new ServiceException(e.getMessage());
+        }
+    }
+
 
     /**
      * 生成验证码
