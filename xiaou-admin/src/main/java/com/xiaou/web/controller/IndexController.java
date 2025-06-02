d67715b732b235957beeebc9293ca90e700f7daa
aliyun1153623907 <3153566913@qq.com>
2025-06-02 12:31:36 +0800

    refactor(xiaou-admin):简化 IndexController 类
    - 移除了未使用的 RequestMappingHandlerMapping 注入
    - 删除了多余的空行和注释

--- xiaou-admin/src/main/java/com/xiaou/web/controller/IndexController.java
+++ xiaou-admin/src/main/java/com/xiaou/web/controller/IndexController.java
@@ -1,22 +1,17 @@
 package com.xiaou.web.controller;
 
 
-
 import cn.hutool.extra.spring.SpringUtil;
 import com.xiaou.common.utils.StringUtils;
-import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @Slf4j
 @RestController
 public class IndexController {
 
 
-    @Resource
-    private RequestMappingHandlerMapping handlerMapping;
 
     @GetMapping("/")
     public String index() {
