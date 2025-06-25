a368336d57f4b13acc6ad5fca70a19a1990d7c5f
aliyun1153623907 <3153566913@qq.com>
2025-06-25 14:18:18 +0800

    feat(security): 实现基于 Sa-Token 的权限控制
    - 在 ApiPrefixConfig 中添加 Sa-Token拦截器，用于登录校验
    - 在 GlobalExceptionHandler 中添加对 NotRoleException 的处理
    -引入 hutool-core 依赖，用于权限控制相关功能

--- xiaou-common/xiaou-common-core/src/main/java/com/xiaou/common/exception/GlobalExceptionHandler.java
+++ xiaou-common/xiaou-common-core/src/main/java/com/xiaou/common/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.xiaou.common.exception;
 
 
 import cn.dev33.satoken.exception.NotLoginException;
+import cn.dev33.satoken.exception.NotRoleException;
 import com.xiaou.common.domain.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,7 +14,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
-
 
 
     @ExceptionHandler(BusinessException.class)
@@ -32,6 +32,12 @@ public class GlobalExceptionHandler {
     public R<?> ServiceExceptionHandler(ServiceException e) {
         log.error(e.getMessage());
         return R.fail(e.getMessage());
+    }
+
+    @ExceptionHandler(NotRoleException.class)
+    public R<?> notRoleExceptionHandler(NotRoleException e) {
+        log.error("NotRoleException", e);
+        return R.fail("无权限");
     }
 
 }
--- xiaou-common/xiaou-common-web/src/main/java/com/xiaou/config/ApiPrefixConfig.java
+++ xiaou-common/xiaou-common-web/src/main/java/com/xiaou/config/ApiPrefixConfig.java
@@ -1,8 +1,13 @@
 package com.xiaou.config;
 
+import cn.dev33.satoken.exception.NotLoginException;
 import cn.dev33.satoken.interceptor.SaInterceptor;
+import cn.dev33.satoken.stp.StpUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.xiaou.common.utils.ServletUtils;
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,20 +16,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
+@Configuration
 public class ApiPrefixConfig implements WebMvcConfigurer {
 
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8848","http://localhost:5174","http://localhost:5173") // 指定前端地址
+                .allowedOrigins("http://localhost:8848", "http://localhost:5174", "http://localhost:5173") // 指定前端地址
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true); // 允许携带凭证
     }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        // 注册 Sa-Token 拦截器，打开注解式鉴权功能
-        registry.addInterceptor(new SaInterceptor()).addPathPatterns("/**");
+        // 注册 Sa-Token 拦截器，校验规则为 StpUtil.checkLogin() 登录校验。
+        registry.addInterceptor(new SaInterceptor(handle -> StpUtil.checkLogin()))
+                .addPathPatterns("/**")
+                .excludePathPatterns("/student/login","/admin/login","/koTime/**");
     }
+
 }
+
