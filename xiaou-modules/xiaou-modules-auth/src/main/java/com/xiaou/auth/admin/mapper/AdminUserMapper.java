9779b2d031eb1fd7e31dc17b51c229d271bd10d1
aliyun1153623907 <3153566913@qq.com>
2025-06-29 12:17:41 +0800

    build(xiaou): 更新 Sa-Token 版本并调整相关依赖- 将 Sa-Token 版本从 1.43.0 升级到 1.44.0 - 移除 sa-token-alone-redis 依赖- 更新 Redis 依赖版本 - 调整项目结构，移动 USpaceApplication 类

--- xiaou-modules/xiaou-modules-auth/src/main/java/com/xiaou/auth/admin/mapper/AdminUserMapper.java
+++ xiaou-modules/xiaou-modules-auth/src/main/java/com/xiaou/auth/admin/mapper/AdminUserMapper.java
@@ -3,8 +3,9 @@ package com.xiaou.auth.admin.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.xiaou.auth.admin.domain.entity.AdminUser;
+import org.apache.ibatis.annotations.Mapper;
 
-
+@Mapper
 public interface AdminUserMapper extends BaseMapper<AdminUser> {
 
 }
