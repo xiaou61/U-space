e55a1489a3ee45a20c4af38507489e158702fd8c
aliyun1153623907 <3153566913@qq.com>
2025-07-01 16:40:00 +0800

    feat(online): 添加在线用户相关接口
    - 新增 OnlineController 类，实现在线用户相关接口
    - 在 LoginHelper 中添加 getUserRoleByIds 方法，用于批量获取用户权限
    - 实现了获取在线用户列表、检查用户是否在线和手动标记下线等功能

--- xiaou-common/xiaou-common-satoken/src/main/java/com/xiaou/satoken/utils/LoginHelper.java
+++ xiaou-common/xiaou-common-satoken/src/main/java/com/xiaou/satoken/utils/LoginHelper.java
@@ -7,6 +7,8 @@ import com.xiaou.satoken.mapper.UserRolesMapper;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class LoginHelper {
 
@@ -49,4 +51,13 @@ public class LoginHelper {
         StpUtil.logout();
     }
 
+    /**
+     * 根据id批量获得用户权限
+     * @param allOnlineUserIds
+     */
+    public void getUserRoleByIds(List<String> allOnlineUserIds) {
+        QueryWrapper<UserRoles> queryWrapper = new QueryWrapper<>();
+        queryWrapper.in("id", allOnlineUserIds);
+        userRolesMapper.selectList(queryWrapper);
+    }
 }
