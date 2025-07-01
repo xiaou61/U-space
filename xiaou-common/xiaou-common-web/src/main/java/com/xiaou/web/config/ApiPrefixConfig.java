01e46e85475f8d9cba5b544fd9e80ae58aaa65a1
aliyun1153623907 <3153566913@qq.com>
2025-07-01 16:39:50 +0800

    feat(web): 添加在线用户自动续命功能
    - 在 ApiPrefixConfig 中添加 SaInterceptor 拦截器
    - 拦截器中增加在线用户自动续命逻辑
    - 更新排除路径，仅排除 /admin/auth 和 /student/auth- 添加 xiaou-common-online依赖

--- xiaou-common/xiaou-common-web/src/main/java/com/xiaou/web/config/ApiPrefixConfig.java
+++ xiaou-common/xiaou-common-web/src/main/java/com/xiaou/web/config/ApiPrefixConfig.java
@@ -2,8 +2,10 @@ package com.xiaou.web.config;
 
 //import cn.dev33.satoken.interceptor.SaInterceptor;
 //import cn.dev33.satoken.stp.StpUtil;
+
 import cn.dev33.satoken.interceptor.SaInterceptor;
 import cn.dev33.satoken.stp.StpUtil;
+import com.xiaou.online.manager.OnlineUserManager;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -24,11 +26,15 @@ public class ApiPrefixConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        // 注册 Sa-Token 拦截器，校验规则为 StpUtil.checkLogin() 登录校验。
-        registry.addInterceptor(new SaInterceptor(handle -> StpUtil.checkLogin()))
+        registry.addInterceptor(new SaInterceptor(handle -> {
+                    StpUtil.checkLogin();
+                    String userId = StpUtil.getLoginIdAsString();
+                    OnlineUserManager.markOnline(userId); // 自动续命
+                }))
                 .addPathPatterns("/**")
-                .excludePathPatterns("/student/login","/admin/login","/koTime/**");
+                .excludePathPatterns("/admin/auth", "/student/auth");
     }
+
 
 }
 
