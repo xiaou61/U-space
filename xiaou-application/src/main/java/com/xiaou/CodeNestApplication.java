836510c04f101daa73eb29218e9b92095cf98024
lzf <3153566913@qq.com>
2025-08-31 10:57:00 +0800

    init

---
+++ xiaou-application/src/main/java/com/xiaou/CodeNestApplication.java
@@ -0,0 +1,12 @@
+package com.xiaou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CodeNestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CodeNestApplication.class, args);
+    }
+}
